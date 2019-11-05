2660e00ae489e4b354e772fbe7beacb06f155a40
Amy Kondro <ajkondro@gmail.com>
2019-11-05 17:08:36 +0000

    added CC4-Ex02

--- CC4/CC4-Ex02/code.py
+++ CC4/CC4-Ex02/code.py
@@ -1,7 +1,8 @@
 # Define function called random_turtle
-def random_turtle()
+def random_turtle():
 # Import the random module as turtle:
-    import turtle
+    import random as turtle 
 # Use randint to produce a random integer between
 # 1 and 10 and save it to a variable called answer
-    answer = randint(1,10)
+    answer = turtle.randint(1,10)
+    return answer
