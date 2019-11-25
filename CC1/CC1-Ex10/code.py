11c2db75133577ddfdf254535defa0a84951acb7
Amy Kondro <ajkondro@gmail.com>
2019-11-25 02:13:20 +0000

    Added CC1-Ex10

--- CC1/CC1-Ex10/code.py
+++ CC1/CC1-Ex10/code.py
@@ -1,5 +1,4 @@
 Celsius = input('What is the degree in C? ')
 Celsius_float = float(Celsius)
 Fahrenheit = (Celsius_float*9/5)+32
-Fahrenheit_float = float(Fahrenheit)
-print('Fahrenheit temp is: ',Fahrenheit,"F")
+print('Fahrenheit temp is:',Fahrenheit)
