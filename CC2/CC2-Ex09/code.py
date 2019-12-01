c2d5a6bea239e9f4fa10d4a4b8624cd639def1a9
Amy Kondro <ajkondro@gmail.com>
2019-12-01 23:44:49 +0000

    added CC2-Ex09

--- CC2/CC2-Ex09/code.py
+++ CC2/CC2-Ex09/code.py
@@ -1,13 +1,17 @@
-hours = input('enter your hours: ')
-rate = input('enter your rate: ')
-hours_float = float(hours)
-rate_float = float(rate)
+try:
+    hours = input('enter your hours: ')
+    rate = input('enter your rate: ')
+    hours_float = float(hours)
+    rate_float = float(rate)
 
-if hours_float > 40 :
-    oth = hours_float - 40
-    otr = oth * (rate_float * 1.5)
-    pay = otr + (hours_float * rate_float)
+    if hours_float > 40 :
+        oth = hours_float - 40
+        otr = oth * (rate_float * 1.5)
+        pay = otr + (40 * rate_float)
 
-else:
-    pay = (hours_float * rate_float)
-print("Pay:",pay)
+    else:
+        pay = (hours_float * rate_float)
+    print('Pay:',pay)
+except:
+    print('Error, pleaase enter numeric input')
+    quit()
