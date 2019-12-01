444333103ab0fbce2554a0be0104f0e446a264a1
Amy Kondro <ajkondro@gmail.com>
2019-12-01 23:07:26 +0000

    added CC2-Ex08

--- CC2/CC2-Ex08/code.py
+++ CC2/CC2-Ex08/code.py
@@ -1,9 +1,9 @@
 height = input('What is your height in inches? ')
-height_int = int(height)
-
-if height_int > 48 :
-    print('Welcome aboard, enjoy the ride!')
-elif height_int < 48 :
-    print('Sorry, you are not tall enough for this ride')
+if height.isdigit():
+    height_int = int(height)
+    if height_int >= 48 :
+        print('Welcome aboard, enjoy the ride!')
+    else:
+        print('Sorry, you are not tall enough for this ride')
 else :
     print('That is not a valid number')
