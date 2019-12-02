14475b6c1aa7bdc7d111f370b69026ee446d5901
Amy Kondro <ajkondro@gmail.com>
2019-12-02 00:26:42 +0000

    added CC2-Ex12

--- CC2/CC2-Ex12/code.py
+++ CC2/CC2-Ex12/code.py
@@ -1,6 +1,4 @@
-command = input('What is the command? ')
-sit = ('puppy dog sits')
-while command != sit :
-    print(command)
-if command == sit:
-    print(sit)
+command = ''
+while command != 'sit' :
+    command = input('What is the command? ')
+print('*puppy dog sits*')
