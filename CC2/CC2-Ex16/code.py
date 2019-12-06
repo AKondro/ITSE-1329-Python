6462b3cc19c40b567a21af0b07272cc39f3802d2
Amy Kondro <ajkondro@gmail.com>
2019-12-06 02:41:12 +0000

    added CC2-Ex16, CC-EX17

--- CC2/CC2-Ex16/code.py
+++ CC2/CC2-Ex16/code.py
@@ -2,7 +2,7 @@ grades = [98, 79, 85, 75, 93, 63, 82, 90, 82, 84, 74]
 
 highest_so_far = -1
 
-for the_num in [98, 79, 85, 75, 93, 63, 82, 90,82, 84, 74]:
+for the_num in grades:
     if the_num > highest_so_far:
         highest_so_far = the_num
     
--- CC2/CC2-Ex17/code.py
+++ CC2/CC2-Ex17/code.py
@@ -2,8 +2,8 @@ grades = [98, 79, 85, 75, 93, 63, 82, 90, 82, 84, 74]
 
 lowest_so_far = 100
 
-for the_num in [98, 79, 85, 75, 93, 63, 82, 90, 82, 84, 74]:
-    if the_num > lowest_so_far:
+for the_num in grades:
+    if the_num < lowest_so_far:
         lowest_so_far = the_num
 
 print('Lowest grade is', lowest_so_far)
